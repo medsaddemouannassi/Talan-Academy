bcc9880bc60ca873fd96ad5765d8295e88869e39
medsaddemouannassi <60628675+medsaddemouannassi@users.noreply.github.com>
2022-04-18 00:54:16 +0100

    Update GroupePrédateursAction.java

--- Act-1.8/src/GroupePrédateursAction.java
+++ Act-1.8/src/GroupePrédateursAction.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public interface GroupePrédateursAction {
     public void afficherCarac();
-    public void afficherCaracPredateurs(ArrayList<Lion> lionGroup);
+    public void afficherCaracPredateurs();
     public void ajouterPredateur(ArrayList groupe, Prédateur prédateur);
     public void enleverPredateur(ArrayList groupe, Prédateur prédateur);
 }
