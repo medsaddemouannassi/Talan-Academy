822c90fcbd0fc889f299358726e30fe9de0cd8ad
medsaddemouannassi <60628675+medsaddemouannassi@users.noreply.github.com>
2022-04-25 01:25:50 +0100

    Add files via upload

---
+++ Act-2.6/Vintud/src/com/vintud/User.java
@@ -0,0 +1,12 @@
+package com.vintud;
+
+public interface User {
+    //void searchAnnouncement();
+    //void filterAnnouncementsPrice();
+    //void filterAnnouncementsLocalisation();
+    //void bookingArticle();
+    //void saveFavoriteAnnouncement();
+    //void displayAnnouncementsViews();
+
+    boolean similar(User user);
+}
