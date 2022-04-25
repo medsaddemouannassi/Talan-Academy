1da5b1d930acc2f6c20ca479454329e4478a4c7c
medsaddemouannassi <60628675+medsaddemouannassi@users.noreply.github.com>
2022-04-25 01:19:49 +0100

    Delete Act-2.4/Gestion de bibliothèque directory

--- Act-2.4/Gestion de bibliothèque/src/com/bibliotehque/BUR.java
+++
@@ -1,9 +0,0 @@
-package com.bibliotehque;
-
-public interface BUR {
-    public void display();
-    public default boolean similarity(Object object) {
-        if (object.equals(this)) return true;
-        return false;
-    }
-}
