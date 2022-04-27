7a615ec11b7e1d02a0528507fd505e7d41f3e110
medsaddemouannassi <60628675+medsaddemouannassi@users.noreply.github.com>
2022-04-27 03:48:44 +0100

    Delete Act-2.8/Vintud - Design Patterns directory

--- Act-2.8/Vintud - Design Patterns/src/com/vintud/FavoriteDao.java
+++
@@ -1,8 +0,0 @@
-package com.vintud;
-
-public interface FavoriteDao {
-    void createFavorite(Favorite favoris);
-    void deleteFavorite(int id);
-    Favorite searchFavorite(AnnouncementImpl announcement);
-    void updateFavorite(Favorite favoris);
-}
