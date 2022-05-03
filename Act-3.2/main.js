16e96eb91a575e639a48168ffa27db59719dc50c
medsaddemouannassi <60628675+medsaddemouannassi@users.noreply.github.com>
2022-05-04 00:23:44 +0100

    Add files via upload

---
+++ Act-3.2/main.js
@@ -0,0 +1,7 @@
+alert("Hello World");
+$(document).ready(function () {
+  $("body").append(" jQuery is installed.");
+  $("form div").css("margin", "17px 0");
+  $("form").css("font-family", "Trebuchet MS");
+  $("label").css({"display": "inline-block", "width": "11rem"});
+});
