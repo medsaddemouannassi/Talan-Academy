a2f5a044129746ae5d69f9efed7bd99431f74825
medsaddemouannassi <60628675+medsaddemouannassi@users.noreply.github.com>
2022-05-15 14:00:24 +0100

    Add files via upload

---
+++ Act-4.4/mvn_project_1/first-maven-project/src/main/java/com/thp/mvn/App.java
@@ -0,0 +1,13 @@
+package com.thp.mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
