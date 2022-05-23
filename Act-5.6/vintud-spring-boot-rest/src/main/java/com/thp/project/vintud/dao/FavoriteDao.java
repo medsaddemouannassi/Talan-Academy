4048ce7fa9fc193cf826f028585f3434f5115e83
medsaddemouannassi <60628675+medsaddemouannassi@users.noreply.github.com>
2022-05-23 23:14:30 +0100

    Delete Act-5.6/vintud-spring-boot-rest/src directory

--- Act-5.6/vintud-spring-boot-rest/src/main/java/com/thp/project/vintud/dao/FavoriteDao.java
+++
@@ -1,9 +0,0 @@
-package com.thp.project.vintud.dao;
-
-import com.thp.project.vintud.entity.impl.Favorite;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FavoriteDao extends JpaRepository<Favorite, Integer> {
-}
