e7e72850c0fb1902df0cafa309374fa08ea5d4ce
medsaddemouannassi <60628675+medsaddemouannassi@users.noreply.github.com>
2022-05-30 01:51:48 +0100

    Delete Act-5.10 directory

--- Act-5.10/vintud-security/src/main/java/com/thp/project/vintud/entity/impl/package-info.java
+++
@@ -1,7 +0,0 @@
-@TypeDef(
-        name = "status", typeClass = PostgreSQLEnumType.class
-)
-package com.thp.project.vintud.entity.impl;
-
-import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
-import org.hibernate.annotations.TypeDef;
